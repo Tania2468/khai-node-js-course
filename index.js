fc06633cc3bfe5d8b1a07e5c9aefa95554f3b8fa
Tatiana <t.v.dyvuljsjka@student.khai.edu>
2024-12-19 23:36:53 +0200

    add middleware and other files for 6th lab

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 const express = require('express');
+const productRoutes = require('./products.routes');
+const { logRequest } = require('./middleware');
+const { errorResponder } = require('./error.middleware');
+
 const app = express();
-
-app.get('/', (request, response) => {
-  response.send('response for GET request');
-});
-
-const products = [
-   { id: 1, name: 'Product 1', brand: 'Brand A'},
-   { id: 2, name: 'Product 2', brand: 'Brand B'},
-   { id: 3, name: 'Product 3', brand: 'Brand C'}
-]
-
-app.get('/products/brand/:brand', (req, res) => {
-   const { brand } = req.params;
-   const filteredProducts = products.filter(product => product.brand === brand);
-   res.json(filteredProducts);
-});
-
-app.get('/products/id/:id', (req, res) => {
-   const { id } = req.params;
-   const filteredProducts = products.filter(product => product.id === parseInt(id, 10));
-   res.json(filteredProducts);
-});
-
 const PORT = 3000;
-app.listen(PORT, 
-   () => console.log(`server start at http://localhost:${PORT}/`));
+
+app.use(logRequest);
+
+app.use(productRoutes);
+
+app.use(errorResponder);
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
